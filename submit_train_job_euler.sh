9f241778ca351f9998c488ef48db3ce54414d0c6
Nikola Popovic <npopovic3794@gmail.com>
2021-04-22 18:28:03 +0200

    ...

--- submit_train_job_euler.sh
+++ submit_train_job_euler.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #BSUB -o /cluster/home/nipopovic/job_logs  # path to output file
 #BSUB -W 71:59 # HH:MM runtime
-#BSUB -n 16 # number of cpu cores
-#BSUB -R "rusage[mem=8192]" # MB per CPU core
+#BSUB -n 32 # number of cpu cores
+#BSUB -R "rusage[mem=4096]" # MB per CPU core
 #BSUB -R "rusage[ngpus_excl_p=4]" # number of GPU cores
 #BSUB -R "select[gpu_mtotal0>=10240]" # MB per GPU core
 
@@ -22,7 +22,7 @@ echo "CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
 
 echo "Number of CPU threads/core: $(nproc --all)"
 
-tar -I pigz -xvf /cluster/work/cvl/yawli/data/ILSVRC2012.tar.gz -C ${TMPDIR}/
+tar -I pigz -xf /cluster/work/cvl/yawli/data/ILSVRC2012.tar.gz -C ${TMPDIR}/
 
 # Set paths
 PROJECT_ROOT_DIR=/cluster/project/cvl/nipopovic/code/pytorch-image-models
@@ -33,5 +33,5 @@ pwd
 #export OMP_NUM_THREADS=8
 
 # python -u run_experiment/imitation_learning/main.py "$@"
-./distributed_train.sh 4 ${TMPDIR}/ILSVRC2012
+./distributed_train.sh 4 
 # CUDA_LAUNCH_BLOCKING=1 for debugging cuda errors
